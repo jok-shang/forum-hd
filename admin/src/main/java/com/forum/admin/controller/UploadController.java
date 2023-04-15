c4745f0f0d8247666fcdd8e55a390bfbf932bb8b
chuanzhang <2859308825@qq.com>
2023-04-15 18:46:37 +0800

    修改上传七牛图片返回值2

--- admin/src/main/java/com/forum/admin/controller/UploadController.java
+++ admin/src/main/java/com/forum/admin/controller/UploadController.java
@@ -2,13 +2,14 @@ package com.forum.admin.controller;
 
 import com.forum.admin.service.UploadImageService;
 import com.forum.common.result.Result;
-import com.forum.model.vo.ResultImage;
-import com.forum.model.vo.ResultImageFail;
+import com.forum.model.vo.WangEditorVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * @auther 尚智江
@@ -51,20 +52,19 @@ public class UploadController {
     }
 
     @PostMapping("/wzuploadImage")
-    public ResultImage WzUploadImage(@RequestParam("file") MultipartFile file) {
+    public WangEditorVO WzUploadImage(@RequestParam("file") MultipartFile file) {
+        List<String> list = new ArrayList<>();
         HashMap<String,Object> map = new HashMap<>();
         if (!file.isEmpty()){
             String path = uploadImageService.uploadQNImg(file);
             if (path.equals("失败")){
-                ResultImage resultImage = new ResultImage(3,"上传失败");
-                return resultImage;
+                return WangEditorVO.error(3,"上传失败");
             }else {
-                map.put("url",path);
-                ResultImage resultImage = new ResultImage(0,map);
-                return resultImage;
+                list.add(path);
+                return WangEditorVO.success(list);
             }
         }
-        return new ResultImage(3,"上传失败");
+        return WangEditorVO.error(3,"上传失败");
     }
 
 }
